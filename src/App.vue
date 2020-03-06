7b3f56606946d4ef92d172d7ea8257f06b4ccd75
ASKJR <albertokatojr@gmail.com>
2020-03-05 21:38:55 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <br />
+    <br />
+    <app-header></app-header>
+  </div>
+</template>
+
+<script>
+import Header from "./components/header/Header.vue";
+export default {
+  components: {
+    appHeader: Header
+  }
+};
+</script>
+
+<style>
+</style>
