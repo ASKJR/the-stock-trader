d18e98a61e145609f169f3e53a40a047340377a0
ASKJR <albertokatojr@gmail.com>
2020-03-05 22:06:10 -0300

    Adding Home page

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,17 @@
     <br />
     <br />
     <app-header></app-header>
+    <app-home></app-home>
   </div>
 </template>
 
 <script>
 import Header from "./components/header/Header.vue";
+import Home from "./components/Home.vue";
 export default {
   components: {
-    appHeader: Header
+    appHeader: Header,
+    appHome: Home
   }
 };
 </script>
---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="panel panel-default">
+    <div class="panel-body">
+      <h1>
+        <b>Trade or View your Portfolio</b>
+      </h1>
+      <p>You may Save & Load you Data</p>
+      <p>Click on 'End Day' to begin a new Day!</p>
+      <hr />
+      <h4>
+        <b>Your Funds: $10.000</b>
+      </h4>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
