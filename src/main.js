b523b3903de3b8fe8da774152aa296aa7d2a0e44
ASKJR <albertokatojr@gmail.com>
2020-03-07 21:13:30 -0300

    installing and cofig of vuex

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import { routes  } from './routes'
+import Vuex from 'vuex'
 
 Vue.use(VueRouter)
+Vue.use(Vuex)
 
 const router = new VueRouter({
     routes,
